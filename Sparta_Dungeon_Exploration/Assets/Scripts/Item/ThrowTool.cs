cd80a0e25a6af3425286510935c7e4f33e7f8d81
mak2u-park <scorio1234@gmail.com>
2025-05-23 15:49:47 +0900

    fix : 불필요한 텍스트 삭제

--- Sparta_Dungeon_Exploration/Assets/Scripts/Item/ThrowTool.cs
+++ Sparta_Dungeon_Exploration/Assets/Scripts/Item/ThrowTool.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class ThrowTool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 던진 오브젝트가 맵 오브젝트와 상호작용하고 플레이어가 이를 밟을 수 있도록 구현
 }
